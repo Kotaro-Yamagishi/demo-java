8909cd5539e35ad3d7db712265b3aa1437431da0
Kotaro-Yamagishi <koutannn772@gmail.com>
2024-09-26 19:02:12 +0900

    add log function

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+
+		// INFOレベルのログを出力
+		logger.info("This is an INFO message");
+
+		// WARNレベルのログを出力
+		logger.warn("This is a WARN message");
+
+		// ERRORレベルのログを出力
+		logger.error("This is an ERROR message");
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
